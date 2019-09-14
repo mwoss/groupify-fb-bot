917e45dccadb5d4c2f210f6ff97de026a36e219d
Mateusz Wos <wos.mateusz16@gmail.com>
2019-09-14 22:42:57 +0200

    Auth endpoint update

--- controllers/authentication.py
+++ controllers/authentication.py
@@ -2,16 +2,16 @@ from os import environ
 
 from flask import Blueprint, request, jsonify
 
-auth_api = Blueprint('auth_api', __name__, url_prefix='/verification')
+auth_api = Blueprint('auth_api', __name__, url_prefix='/webhook')
 
 
 @auth_api.route('/', methods=['GET', 'POST'])
 def verify():
-    hun_challenge = request.args.get('hub.challenge')
-    hub_mode = request.args.get('hun.mode')
+    hub_challenge = request.args.get('hub.challenge')
+    hub_mode = request.args.get('hub.mode')
     is_verified = request.args.get('hub.verify_token') == environ.get("GROUPIFY_TOKEN")
 
     if hub_mode and is_verified:
-        return jsonify(hun_challenge), 200
+        return hub_challenge, 200
 
     return jsonify({'error': 'Non authorized'}), 403
